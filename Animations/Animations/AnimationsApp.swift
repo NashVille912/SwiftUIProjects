b6b321440994170f853bd4ff90988ce7f14c11bb
Nacho Villarraza <143408479+NashVille912@users.noreply.github.com>
2024-02-09 13:59:53 -0300

    Project: Animations, Added

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Nacho on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
